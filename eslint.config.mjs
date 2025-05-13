666ece4e4d37f0ee9ee1e13dfcd1780de399590f
Sena Kaplan <senanebilekaplan@gmail.com>
2025-05-14 01:20:05 +0300

    refactor(workspace): migrate project from backend-only to monorepo structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { antfu } from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  typescript: { tsconfigPath: './tsconfig.json' },
-  ignores: ['prisma'],
+  typescript: false,
+  ignores: ['apps', 'packages'],
 })
