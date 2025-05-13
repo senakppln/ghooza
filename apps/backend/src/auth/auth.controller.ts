2263b595493e158143b3df0dd008f44fbd4911c0
Sena Kaplan <senanebilekaplan@gmail.com>
2025-05-14 01:56:51 +0300

    refactor(schemas): configure schemas package to be usable by the backend

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import { ZodValidationPipe } from '@app/pipes'
-import { Login, loginSchema, SendTempPass, sendTempPassSchema } from '@app/schemas'
 import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common'
 import { Request, Response } from 'express'
 import { AuthService } from './auth.service'
 import { OpenAccess } from './decorator'
+import { Login, loginSchema, SendTempPass, sendTempPassSchema } from 'schemas'
 
 @Controller('auth')
 export class AuthController {
